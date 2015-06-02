000e492abd6ec5446125ec224b9427b3ece4ed94
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-02 19:20:39 +1200

    fiddled with sizing and found an appropriate size. 15px and changed interval to 100

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
-  var conway = new Conway(100);
+  var conway = new Conway(65);
   conway.renderGrid();
+
+  // var windowWidth = $(window).width();
+  // var cellSize = windowWidth / 100;
+  // $('.cell').width(cellSize);
+  // $('.cell').height(cellSize);
+  // $('.row').height(cellSize);
+
   var time = setInterval( function () {
     conway.updateNeighborsForAllCells();
     conway.updateAllCells();
-  }, 50);
+  }, 100);
+
+  // $(window).resize(function() {
+  //   var windowWidth = $(window).width();
+  //   var cellSize = windowWidth / 50;
+  //   $('.cell').width(cellSize);
+  //   $('.cell').height(cellSize);
+  //   $('.row').height(cellSize);
+  // });
+
 });
 
+
+// when we resize the window, we want to change the width and height of each cell,
+// we want to make the row height to equal cell height
